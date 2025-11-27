a7b79107dabee95be30114e93901fd3bb9645a0c
Mateusz Pusz <mateusz.pusz@gmail.com>
2025-11-27 22:28:23 +0100

    Task 34 solution added

--- echo/conanfile.py
+++ echo/conanfile.py
@@ -21,15 +21,23 @@
 # SOFTWARE.
 
 from conan import ConanFile
-from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.build import can_run, check_min_cppstd
 
 
 class EchoConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    test_requires = "gtest/1.10.0"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    no_copy_source = True
+
+    @property
+    def _build_all(self):
+        return bool(self.conf.get("user.workshop.build:all", default=False))
+
+    def build_requirements(self):
+        if self._build_all:
+            self.test_requires("gtest/1.10.0")
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -48,10 +56,19 @@ class EchoConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.cache_variables["CMAKE_MESSAGE_CONTEXT_SHOW"] = True
-        tc.cache_variables["CMAKE_VERIFY_INTERFACE_HEADER_SETS"] = True
+        tc.cache_variables["CMAKE_VERIFY_INTERFACE_HEADER_SETS"] = self._build_all
         tc.cache_variables["CMAKE_EXPERIMENTAL_EXPORT_PACKAGE_INFO"] = (
             "b80be207-778e-46ba-8080-b23bba22639e"
         )
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(build_script_folder=None if self._build_all else "src")
+        cmake.build()
+        if self._build_all:
+            cmake.build(target="all_verify_interface_header_sets")
+            if can_run(self):
+                cmake.ctest(cli_args=["--output-on-failure"])
