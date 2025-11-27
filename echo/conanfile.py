4ffe4580a9abc19ae204a3f5640c819d6b0ce6fb
Mateusz Pusz <mateusz.pusz@gmail.com>
2025-11-27 22:36:39 +0100

    Task 35 solution added

--- echo/conanfile.py
+++ echo/conanfile.py
@@ -20,20 +20,32 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os, re
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.build import can_run, check_min_cppstd
+from conan.tools.files import copy, load, rmdir
 
 
 class EchoConan(ConanFile):
+    name = "echo"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
+    exports = "LICENSE.md"
+    exports_sources = "src/*", "test/*", "cmake/*", "example/*", "CMakeLists.txt"
     no_copy_source = True
 
     @property
     def _build_all(self):
         return bool(self.conf.get("user.workshop.build:all", default=False))
+
+    def set_version(self):
+        content = load(self, os.path.join(self.recipe_folder, "src/CMakeLists.txt"))
+        version = re.search(
+            r"project\([^\)]+VERSION (\d+\.\d+\.\d+)[^\)]*\)", content
+        ).group(1)
+        self.version = version.strip()
 
     def build_requirements(self):
         if self._build_all:
@@ -72,3 +84,24 @@ class EchoConan(ConanFile):
             cmake.build(target="all_verify_interface_header_sets")
             if can_run(self):
                 cmake.ctest(cli_args=["--output-on-failure"])
+
+    def package(self):
+        copy(
+            self,
+            "LICENSE.md",
+            self.source_folder,
+            os.path.join(self.package_folder, "licenses"),
+        )
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
+    def package_info(self):
+        self.cpp_info.components["protocol"].libs = ["echo-protocol"]
+        self.cpp_info.components["server"].requires = ["protocol"]
+        self.cpp_info.components["server"].libs = ["echo-server"]
+        if self.settings.build_type == "Debug":
+            self.cpp_info.components["protocol"].libdirs = ["lib/debug"]
+            self.cpp_info.components["protocol"].bindirs = ["bin/debug"]
+            self.cpp_info.components["server"].libdirs = ["lib/debug"]
+            self.cpp_info.components["server"].bindirs = ["bin/debug"]
